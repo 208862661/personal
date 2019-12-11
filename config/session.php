1aadcee82076c4412a4ece1ec923eee2d27b3acc
richie <richie.li@ecoprint.tech>
2019-12-11 14:49:49 +0800

    初始化为tp6框架

--- config/session.php
+++ config/session.php
@@ -1,33 +1,19 @@
 <?php
 // +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-
-// +----------------------------------------------------------------------
 // | 会话设置
 // +----------------------------------------------------------------------
 
-use think\facade\Env;
 return [
-    'id'             => '',
+    // session name
+    'name'           => 'PHPSESSID',
     // SESSION_ID的提交变量,解决flash上传跨域
     'var_session_id' => '',
-    // SESSION 前缀
-    'prefix'         => 'think',
-    // 驱动方式 支持redis memcache memcached
-    'type'           => Env::get('redis.type',''),
-    // 是否自动开启 SESSION
-    'auto_start'     => true,
-    // redis主机
-    'host'           => Env::get('redis.host',''),
-    // redis端口
-    'port'           => Env::get('redis.port',''),
-    // 密码
-    'password'       => Env::get('redis.password',''),
+    // 驱动方式 支持file cache
+    'type'           => 'file',
+    // 存储连接标识 当type使用cache的时候有效
+    'store'          => null,
+    // 过期时间
+    'expire'         => 1440,
+    // 前缀
+    'prefix'         => '',
 ];
